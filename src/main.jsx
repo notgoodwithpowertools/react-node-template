0ab3c211b48dc5eb8d5aad03214ceac4e9dcd894
notgoodwithpowertools <notgoodwithpowertools@gmail.com>
2016-04-28 08:51:49 +1000

    Updating some core files

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Routes = require('./routes.jsx');
 
-var TestComponent = require('./components/TestComponent.jsx');
+//var TestComponent = require('./components/TestComponent.jsx');
 
-ReactDOM.render(<TestComponent title="Test" />, document.getElementById('test1'));
+//ReactDOM.render(<TestComponent title="Test" />, document.getElementById('test1'));
+ReactDOM.render(Routes, document.getElementById('main'));
